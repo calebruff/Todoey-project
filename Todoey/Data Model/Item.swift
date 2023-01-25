c94feb3f2cf2f495b689fe93ed9d98cf961ce888
Caleb Ruff <crpwwl@gmail.com>
2023-01-25 23:45:18 +0400

    mvc

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Caleb Ruff on 25.01.2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+}

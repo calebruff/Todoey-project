bccf5623ebcf51b7a6391e4273a6892fd71d99ec
Caleb Ruff <crpwwl@gmail.com>
2023-02-05 22:41:58 +0400

    CategoryVC - TableView Datasource Methods

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -11,16 +11,35 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
     
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    //MARK: - TableView Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
     }
     
-    //MARK: - TableView Datasource Methods
     
     //MARK: - TableView Delegate Methods
     
